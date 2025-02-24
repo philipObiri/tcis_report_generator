4c5372810874db855f8c2c52b44d6dd427accacf
Philip Obiri <philipobri3.1@gmail.com>
2025-02-24 20:35:51 +0000

    Registered Teacher's Profile

--- reports/admin.py
+++ reports/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.utils.html import format_html
 from .models import (
     Level, ClassYear, 
-    Term, Subject, Student,
+    Term, Subject, Student,TeacherProfile,
     Score, AcademicReport,MidtermReport, 
     ProgressiveTestOneReport, ProgressiveTestTwoReport, 
     ProgressiveTestThreeReport
@@ -46,6 +46,29 @@ class StudentAdmin(admin.ModelAdmin):
     search_fields = ('fullname', 'class_year__name')
     list_filter = ('class_year',)
     filter_horizontal = ('subjects',)
+
+
+
+# TeacherProfile Admin
+class TeacherProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'get_subjects', 'get_class_years')
+    search_fields = ('user__username', 'user__first_name', 'user__last_name')
+    list_filter = ('subjects',)
+    filter_horizontal = ('subjects',)
+    
+    def get_subjects(self, obj):
+        return ", ".join([subject.name for subject in obj.subjects.all()])
+    get_subjects.short_description = 'Subjects'
+
+    def get_class_years(self, obj):
+        class_years = set()
+        for subject in obj.subjects.all():
+            for class_year in subject.class_year.all():
+                class_years.add(class_year.name)  # Assuming 'name' is a field of ClassYear
+        return ", ".join(class_years)
+    get_class_years.short_description = 'Class Years'
+
+
 
 
 # Register the Score model
@@ -111,6 +134,7 @@ admin.site.register(Level, LevelAdmin)
 admin.site.register(ClassYear, ClassYearAdmin)
 admin.site.register(Term, TermAdmin)
 admin.site.register(Student, StudentAdmin)
+admin.site.register(TeacherProfile, TeacherProfileAdmin)
 admin.site.register(Subject, SubjectAdmin)
 admin.site.register(Score, ScoreAdmin)
 # admin.site.register(MidtermReport, MidtermReportAdmin)
